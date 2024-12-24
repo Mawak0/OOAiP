6d21ea4f9f59379adabc2e1fc2e7adcd31023bbf
Anastasia Essina <aaessina@yandex.ru>
2024-12-24 17:20:30 +0600

    feat: InjectableCommand added

---
+++ SpaceBattle.Lib/CommandInjectableCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace SpaceBattle.Lib;
+
+public class InjectableCommand : ICommand, ICommandInjectable
+{
+    private ICommand _cmd = new InvalidCommand();
+    public void Execute()
+    {
+        _cmd.Execute();
+    }
+    public void Inject(ICommand cmd)
+    {
+        _cmd = cmd;
+    }
+}
