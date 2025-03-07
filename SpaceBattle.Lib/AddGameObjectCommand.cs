8a0ef03e20738177a97f32f5ac1e5db55c251447
Mawak0 <a32221580@gmail.com>
2025-03-07 23:24:20 +0600

    feat: GameObjectRepository added

---
+++ SpaceBattle.Lib/AddGameObjectCommand.cs
@@ -0,0 +1,18 @@
+﻿using App;
+
+namespace SpaceBattle.Lib;
+
+public class AddGameObjectCommand : ICommand
+{
+    private readonly object ObjToAdd;
+    private readonly string uuid;
+    public AddGameObjectCommand(string uuid, object ObjToAdd)
+    {
+        this.uuid = uuid;
+        this.ObjToAdd = ObjToAdd;
+    }
+    public void Execute()
+    {
+        Ioc.Resolve<IDictionary<string, object>>("Game.Object.Repository").Add(uuid, ObjToAdd);
+    }
+}
---
+++ SpaceBattle.Lib/RegisterIoCDependencyAddGameObject.cs
@@ -0,0 +1,14 @@
+﻿using App;
+
+namespace SpaceBattle.Lib;
+
+public class RegisterIoCDependencyAddGameObject : ICommand
+{
+    public void Execute()
+    {
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "Game.Object.Add",
+            (object[] args) => new AddGameObjectCommand((string)args[0], args[1])).Execute();
+    }
+}
