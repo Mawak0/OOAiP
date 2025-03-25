74c36cee72f41c014ddc8480501f4cafd9084562
bunpdy <bunpdyz@mail.ru>
2025-03-25 11:57:08 +0600

    AuthCommand added

---
+++ SpaceBattle.Lib.Tests/AuthCommandTest.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+namespace SpaceBattle.Lib.Tests;
+using App;
+using App.Scopes;
+
+public class AuthTests
+{
+    public AuthTests()
+    {
+        new InitCommand().Execute();
+        var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+        Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+    }
+
+    [Fact]
+    public void AuthCommandSuccess()
+    {
+        RegisterDependencies();
+
+        var authcommand = Ioc.Resolve<AuthCommand>("Commands.Auth", "Player1", "Fire", "Spaceship3");
+
+        var exception = Record.Exception(() => authcommand.Execute());
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void AuthCommandException()
+    {
+        RegisterDependencies();
+
+        var authcommand = Ioc.Resolve<AuthCommand>("Commands.Auth", "Player1", "Fire", "Spaceship1");
+
+        var exception = Assert.Throws<Exception>(() => authcommand.Execute());
+        Assert.Equal("Сommand not allowed", exception.Message);
+    }
+
+    internal static void RegisterDependencies()
+    {
+        string username = "Player1";
+        string command = "Fire";
+        string[] objects = ["Spaceship4", "Spaceship3", "Spaceship2"];
+
+        var registerIoCDependencyLegalOrderRepository = new RegisterIoCDependencyLegalOrderRepository();
+        registerIoCDependencyLegalOrderRepository.Execute();
+
+        var registerIoCDependencyAddLegalOrder = new RegisterIoCDependencyAddLegalOrder();
+        registerIoCDependencyAddLegalOrder.Execute();
+
+        var addorder = Ioc.Resolve<AddLegalOrder>("LegalOrder.Add", username, command, objects);
+        addorder.Execute();
+
+        var registerDependencyAuthCommand = new RegisterDependencyAuthCommand();
+        registerDependencyAuthCommand.Execute();
+    }
+}
---
+++ SpaceBattle.Lib/AuthCommand.cs
@@ -0,0 +1,31 @@
+﻿namespace SpaceBattle.Lib;
+using App;
+
+public class AuthCommand : ICommand
+{
+    private readonly string user;
+    private readonly string command;
+    private readonly string obj;
+    public AuthCommand(string user_, string command_, string obj_)
+    {
+        user = user_;
+        command = command_;
+        obj = obj_;
+    }
+    public void Execute()
+    {
+        var legalOrdersRepository = Ioc.Resolve<Dictionary<string, Dictionary<string, string[]>>>("LegalOrder.Repository");
+        bool flagCommandCheck = false;
+
+        if (legalOrdersRepository.ContainsKey(user) && legalOrdersRepository[user].ContainsKey(command))
+        {
+            legalOrdersRepository[user].TryGetValue(command, out string[]? objects);
+            flagCommandCheck = objects!.Any(p => p == obj);
+        }
+
+        if (!flagCommandCheck)
+        {
+            throw new Exception("Сommand not allowed");
+        }
+    }
+}
