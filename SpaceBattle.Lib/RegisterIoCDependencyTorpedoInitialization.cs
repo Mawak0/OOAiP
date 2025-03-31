d3ee4de30548a0a91bf27caa061805294dca056e
Anastasia Essina <aaessina@yandex.ru>
2025-03-31 15:01:38 +0600

    feat: TorpedoInitialization add

---
+++ SpaceBattle.Lib/RegisterIoCDependencyTorpedoInitialization.cs
@@ -0,0 +1,14 @@
+﻿using App;
+namespace SpaceBattle.Lib;
+
+public class RegisterIoCDependencyTorpedoInitialization : ICommand
+{
+    public void Execute()
+    {
+        Ioc.Resolve<App.ICommand>(
+                "IoC.Register",
+                "Game.TorpedoInitialization",
+                (object[] arg) => new TorpedoInitialization((IDictionary<string, object>)arg[0], (IShooting)arg[1])
+        ).Execute();
+    }
+}
---
+++ SpaceBattle.Lib/TorpedoInitialization.cs
@@ -0,0 +1,18 @@
+﻿namespace SpaceBattle.Lib;
+
+public class TorpedoInitialization : ICommand
+{
+    private readonly IDictionary<string, object> torpedo;
+    private readonly IShooting shooterObj;
+    public TorpedoInitialization(IDictionary<string, object> torpedo, IShooting shooterObj)
+    {
+        this.torpedo = torpedo;
+        this.shooterObj = shooterObj;
+    }
+    public void Execute()
+    {
+        torpedo["Position"] = shooterObj.Position + shooterObj.FireVelocity;
+        torpedo["Velocity"] = shooterObj.Velocity + shooterObj.FireVelocity;
+
+    }
+}
