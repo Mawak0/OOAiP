a6e02b4ba898e81ca1cbb4fe88f0eb879da3b6f9
Anastasia Essina <aaessina@yandex.ru>
2025-04-08 14:08:36 +0600

    feat: CollisionCommand added

---
+++ SpaceBattle.Lib.Tests/CollisionCommandTests.cs
@@ -0,0 +1,104 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+using Xunit;
+
+namespace SpaceBattle.Lib.Tests
+{
+    public class CollisionCommandTests
+    {
+
+        public CollisionCommandTests()
+        {
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+        }
+        [Fact]
+        public void Execute_CollisionDetected_CommandExecuted()
+        {
+
+            var pos1 = new Vector(new int[] { 10, 20 });
+            var pos2 = new Vector(new int[] { 12, 18 });
+            var vel1 = new Vector(new int[] { 1, -1 });
+            var vel2 = new Vector(new int[] { -1, 1 });
+
+            var mockFirstObj = new Mock<IColliding>();
+            mockFirstObj.SetupGet(o => o.Position).Returns(pos1);
+            mockFirstObj.SetupGet(o => o.Velocity).Returns(vel1);
+            mockFirstObj.SetupGet(o => o.Shape).Returns("Circle");
+
+            var mockSecondObj = new Mock<IColliding>();
+            mockSecondObj.SetupGet(o => o.Position).Returns(pos2);
+            mockSecondObj.SetupGet(o => o.Velocity).Returns(vel2);
+            mockSecondObj.SetupGet(o => o.Shape).Returns("Circle");
+
+            var mockCommand = new Mock<ICommand>();
+
+            Ioc.Resolve<App.ICommand>("IoC.Register", "Game.IsCollision", (object[] args) => (object)true).Execute();
+
+            var collisionCommand = new CollisionCommand(
+                mockFirstObj.Object,
+                mockSecondObj.Object,
+                mockCommand.Object
+            );
+
+            collisionCommand.Execute();
+
+            mockCommand.Verify(c => c.Execute(), Times.Once);
+        }
+
+        [Fact]
+        public void Execute_NoCollisionDetected_CommandNotExecuted()
+        {
+
+            var pos1 = new Vector(new int[] { 10, 20 });
+            var pos2 = new Vector(new int[] { 100, 200 });
+            var vel1 = new Vector(new int[] { 1, -1 });
+            var vel2 = new Vector(new int[] { -1, 1 });
+
+            var mockFirstObj = new Mock<IColliding>();
+            mockFirstObj.SetupGet(o => o.Position).Returns(pos1);
+            mockFirstObj.SetupGet(o => o.Velocity).Returns(vel1);
+            mockFirstObj.SetupGet(o => o.Shape).Returns("Circle");
+
+            var mockSecondObj = new Mock<IColliding>();
+            mockSecondObj.SetupGet(o => o.Position).Returns(pos2);
+            mockSecondObj.SetupGet(o => o.Velocity).Returns(vel2);
+            mockSecondObj.SetupGet(o => o.Shape).Returns("Circle");
+
+            var mockCommand = new Mock<ICommand>();
+
+            Ioc.Resolve<App.ICommand>("IoC.Register", "Game.IsCollision", (object[] args) => (object)false).Execute();
+
+            var collisionCommand = new CollisionCommand(
+                mockFirstObj.Object,
+                mockSecondObj.Object,
+                mockCommand.Object
+            );
+
+            collisionCommand.Execute();
+
+            mockCommand.Verify(c => c.Execute(), Times.Never);
+        }
+
+        [Fact]
+        public void Execute_FirstObjectPositionUnreadable_ThrowsException()
+        {
+
+            var mockFirstObj = new Mock<IColliding>();
+            mockFirstObj.SetupGet(o => o.Position).Throws<InvalidOperationException>();
+
+            var mockSecondObj = new Mock<IColliding>();
+            var mockCommand = new Mock<ICommand>();
+
+            var collisionCommand = new CollisionCommand(
+                mockFirstObj.Object,
+                mockSecondObj.Object,
+                mockCommand.Object
+            );
+
+            Assert.Throws<InvalidOperationException>(() => collisionCommand.Execute());
+        }
+    }
+}
---
+++ SpaceBattle.Lib/CollisionCommand.cs
@@ -0,0 +1,34 @@
+﻿using App;
+namespace SpaceBattle.Lib;
+
+public class CollisionCommand : ICommand
+{
+    private readonly IColliding firstObj;
+
+    private readonly IColliding secondObj;
+
+    private readonly ICommand command;
+
+    public CollisionCommand(IColliding firstObj, IColliding secondObj, ICommand command)
+    {
+        this.firstObj = firstObj;
+        this.secondObj = secondObj;
+        this.command = command;
+    }
+    public void Execute()
+    {
+        var deltaPosition = firstObj.Position.Values
+        .Zip(secondObj.Position.Values, (a, b) => a - b)
+        .ToArray();
+
+        var deltaVelocity = firstObj.Velocity.Values
+        .Zip(secondObj.Velocity.Values, (a, b) => a - b)
+        .ToArray();
+
+        var isCollision = Ioc.Resolve<bool>("Game.IsCollision", deltaPosition, deltaVelocity, firstObj.Shape, secondObj.Shape);
+        if (isCollision)
+        {
+            command.Execute();
+        }
+    }
+}
