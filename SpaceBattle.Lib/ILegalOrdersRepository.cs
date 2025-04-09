7aa1ab40226bf2c5631034abff7c44d82a16fbcb
bunpdy <bunpdyz@mail.ru>
2025-04-09 09:54:08 +0600

    feat: ILegalOrdersRepository added

---
+++ SpaceBattle.Lib/ILegalOrdersRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace SpaceBattle.Lib;
+
+public interface ILegalOrdersRepository
+{
+    public IDictionary<string, IDictionary<string, HashSet<string>>> legalOrdersRepository { get; }
+    public void AddLegalOrder(string user, string command, HashSet<string> obj);
+    public void RemoveLegalOrder(string user, string command, string obj);
+}
