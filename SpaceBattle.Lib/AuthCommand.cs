d639d71f8ac63ff8e283c06743975453c2094b15
MarkSabenin <sabenin-mark@mail.ru>
2025-04-10 20:45:01 +0600

    feat: AuthCommand added

---
+++ SpaceBattle.Lib/AuthCommand.cs
@@ -0,0 +1,32 @@
+﻿namespace SpaceBattle.Lib;
+using App;
+
+public class AuthCommand : ICommand
+{
+    private readonly string user;
+    private readonly string command;
+    private readonly string obj;
+    public AuthCommand(string user_, string command_, string obj_)
+    {
+        user = user_;
+        command = command_;
+        obj = obj_;
+    }
+
+    public void Execute()
+    {
+        var legalOrdersRepository = Ioc.Resolve<ILegalOrdersRepository>("LegalOrders.Repository").legalOrdersRepository;
+
+        bool flagCommandCheck = false;
+        if (legalOrdersRepository.ContainsKey(user) && legalOrdersRepository[user].ContainsKey(command))
+        {
+            legalOrdersRepository[user].TryGetValue(command, out HashSet<string>? objects);
+            flagCommandCheck = objects!.Any(p => p == obj);
+        }
+
+        if (!flagCommandCheck)
+        {
+            throw new Exception("Сommand not allowed");
+        }
+    }
+}
