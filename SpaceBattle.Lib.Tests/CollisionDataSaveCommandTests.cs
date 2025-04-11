908b91bc2e03f34daabdcf57a33911b0957df694
Mawak0 <a32221580@gmail.com>
2025-04-11 10:22:36 +0600

    feat: CollisionDataSaveCommand added

---
+++ SpaceBattle.Lib.Tests/CollisionDataSaveCommandTests.cs
@@ -0,0 +1,47 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+using Xunit;
+
+namespace SpaceBattle.Lib.Tests;
+
+public class CollisionDataSaveCommandTests
+{
+    public CollisionDataSaveCommandTests()
+    {
+        new InitCommand().Execute();
+        var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+        Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+    }
+    [Fact]
+    public void Execute_ShouldWriteFileAndLoadToMemory()
+    {
+        var dummyData = new List<int[]> { new[] { 1, 2 } };
+        var path = "testpath/";
+        var fullPath = path + "mycollision";
+
+        var writeCommand = new Mock<ICommand>();
+        var loadCommand = new Mock<ICommand>();
+
+        Ioc.Resolve<App.ICommand>("IoC.Register", "Data.CollisionFilesPath", (object[] args) => path).Execute();
+        Ioc.Resolve<App.ICommand>("IoC.Register", "Commands.WriteObjectToFile", (object[] args) =>
+        {
+            Assert.Equal(fullPath, args[0]);
+            Assert.Same(dummyData, args[1]);
+            return writeCommand.Object;
+        }).Execute();
+        Ioc.Resolve<App.ICommand>("IoC.Register", "Collision.LoadDataToMemory", (object[] args) =>
+        {
+            Assert.Equal("mycollision", args[0]);
+            Assert.Same(dummyData, args[1]);
+            return loadCommand.Object;
+        }).Execute();
+
+        new RegisterIoCDependencyCollisionDataSaveCommand().Execute();
+        var command = Ioc.Resolve<ICommand>("Collision.DataSaveCommand", "mycollision", dummyData);
+        command.Execute();
+
+        writeCommand.Verify(c => c.Execute(), Times.Once);
+        loadCommand.Verify(c => c.Execute(), Times.Once);
+    }
+}
---
+++ SpaceBattle.Lib/RegisterIoCDependencyCollisionDataSaveCommand.cs
@@ -0,0 +1,13 @@
+﻿using App;
+namespace SpaceBattle.Lib;
+
+public class RegisterIoCDependencyCollisionDataSaveCommand : ICommand
+{
+    public void Execute()
+    {
+        Ioc.Resolve<App.ICommand>(
+                "IoC.Register",
+                "Collision.DataSaveCommand",
+                (object[] arg) => new CollisionDataSaveCommand((string)arg[0], (IList<int[]>)arg[1])).Execute();
+    }
+}
