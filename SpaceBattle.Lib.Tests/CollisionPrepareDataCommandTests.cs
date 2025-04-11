209bd43bf2d2ec5525622b650b323225892aa551
Mawak0 <a32221580@gmail.com>
2025-04-11 10:23:06 +0600

    feat: CollisionPrepareDataCommand added

---
+++ SpaceBattle.Lib.Tests/CollisionPrepareDataCommandTests.cs
@@ -0,0 +1,45 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+using Xunit;
+
+namespace SpaceBattle.Lib.Tests;
+
+public class CollisionPrepareDataCommandTests
+{
+    public CollisionPrepareDataCommandTests()
+    {
+        new InitCommand().Execute();
+        var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+        Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+    }
+
+    [Fact]
+    public void Execute_GeneratesCollisionData_AndCallsSaveCommand()
+    {
+
+        var mockDataGenerator = new Mock<ICollisionDataGenerator>();
+        var mockSaveCommand = new Mock<ICommand>();
+
+        mockDataGenerator
+            .Setup(g => g.GenerateCollisionData())
+            .Returns(new List<int[]> { new int[] { 1, 2, 3, 4 } });
+
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "Collision.DataSaveCommand",
+            (object[] args) => mockSaveCommand.Object
+        ).Execute();
+
+        var regCollisisonPrepareData = new RegisterIoCDependencyCollisionPrepareData();
+        regCollisisonPrepareData.Execute();
+        var regCollisionName = new RegisterIoCDependencyGetCollisionName();
+        regCollisionName.Execute();
+        var collisionPrepareDataCommand = Ioc.Resolve<ICommand>("Collision.PrepareData", mockDataGenerator.Object);
+
+        collisionPrepareDataCommand.Execute();
+
+        mockDataGenerator.Verify(g => g.GenerateCollisionData(), Times.Once);
+        mockSaveCommand.Verify(c => c.Execute(), Times.Once);
+    }
+}
