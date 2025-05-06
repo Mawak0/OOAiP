24a5d4f54be4bb43ffc0f513dad8c2c667adaa9a
MarkSabenin <sabenin-mark@mail.ru>
2025-05-06 15:04:50 +0600

    LegalOrderRepository added

---
+++ SpaceBattle.Lib/AddLegalOrderCommand.cs
@@ -0,0 +1,29 @@
+﻿namespace SpaceBattle.Lib;
+
+using App;
+
+public class AddLegalOrder : ICommand
+{
+    public string user;
+    public string command;
+    public HashSet<string> obj;
+    public AddLegalOrder(string user, string command, HashSet<string> obj)
+    {
+        this.user = user;
+        this.command = command;
+        this.obj = obj;
+    }
+    public void Execute()
+    {
+        System.Diagnostics.Debug.WriteLine("Execute called");
+        var legalOrdersRepository = Ioc.Resolve<Dictionary<string, Dictionary<string, HashSet<string>>>>("LegalOrders.Repository");
+
+        Dictionary<string, HashSet<string>> internal_dict = new Dictionary<string, HashSet<string>>() { { command, obj } };
+        legalOrdersRepository.TryAdd(user, internal_dict);
+
+        legalOrdersRepository[user].TryAdd(command, obj);
+
+        legalOrdersRepository[user].TryGetValue(command, out HashSet<string>? objects);
+        legalOrdersRepository[user][command] = objects!.Union(obj.Except(objects!)).ToHashSet();
+    }
+}
---
+++ SpaceBattle.Lib/RegisterIoCDependencyAddLegalOrderCommand.cs
@@ -0,0 +1,14 @@
+﻿using App;
+namespace SpaceBattle.Lib;
+
+public class RegisterIoCDependencyAddLegalOrderCommand : ICommand
+{
+    public void Execute()
+    {
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "LegalOrders.Add",
+            (object[] args) => new AddLegalOrder((string)args[0], (string)args[1], (HashSet<string>)args[2])
+        ).Execute();
+    }
+}
