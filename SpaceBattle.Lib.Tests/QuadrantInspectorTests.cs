a3437d07eb061eb2d129115850ff9792b0f906e6
ShelDenis <shele1-2010@mail.ru>
2025-05-22 21:12:51 +0600

    feat: quadrant inspector

---
+++ SpaceBattle.Lib.Tests/QuadrantInspectorTests.cs
@@ -0,0 +1,181 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+using Xunit;
+
+namespace SpaceBattle.Lib.Tests;
+
+public class QuadrantInspectorTests
+{
+    public QuadrantInspectorTests()
+    {
+        new InitCommand().Execute();
+        var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+        Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+    }
+
+    [Fact]
+    public void GetQuadrantTest()
+    {
+        var key = "shape";
+
+        var polygon = new Polygon(new List<Point>() { new Point(1, 1), new Point(2, 2), new Point(1, 3) });
+
+        var polygonDict = new Dictionary<string, Polygon> {
+                            { key, polygon }
+                        };
+
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "Collision.GetPolygonDict",
+            (object[] args) => polygonDict
+        ).Execute();
+
+        Ioc.Resolve<App.ICommand>("IoC.Register", "Field.Quadrant.Size", (object[] args) => (object)10).Execute();
+
+        var inspector = new QuadrantInspector();
+        var mockColliding = new Mock<IColliding>();
+
+        mockColliding.SetupGet(o => o.Shape).Returns("shape");
+
+        var result = inspector.getQuadrants(mockColliding.Object);
+
+        Assert.Equal(new List<int[]> { new[] { 0, 0 } }, result.Select(x => x.ToArray()).ToList());
+    }
+
+    [Fact]
+    public void CorrectQuadrantToObjectTest()
+    {
+        var key = "shape";
+
+        var polygon = new Polygon(new List<Point>() { new Point(1, 1), new Point(2, 2), new Point(1, 3) });
+
+        var polygonDict = new Dictionary<string, Polygon> {
+                            { key, polygon }
+                        };
+
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "Collision.GetPolygonDict",
+            (object[] args) => polygonDict
+        ).Execute();
+
+        Ioc.Resolve<App.ICommand>("IoC.Register", "Field.Quadrant.Size", (object[] args) => (object)10).Execute();
+
+        var inspector = new QuadrantInspector();
+        var mockColliding = new Mock<IColliding>();
+
+        mockColliding.SetupGet(o => o.Shape).Returns("shape");
+
+        var isInQuadrant = inspector.InQuadrant(mockColliding.Object, new[] { 0, 0 });
+
+        Assert.True(isInQuadrant);
+    }
+
+    [Fact]
+    public void CorrectSetQuadrantsToObjectTest()
+    {
+        var key = "shape";
+
+        var polygon = new Polygon(new List<Point>() { new Point(1, 1), new Point(2, 2), new Point(11, 3) });
+
+        var polygonDict = new Dictionary<string, Polygon> {
+                            { key, polygon }
+                        };
+
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "Collision.GetPolygonDict",
+            (object[] args) => polygonDict
+        ).Execute();
+
+        Ioc.Resolve<App.ICommand>("IoC.Register", "Field.Quadrant.Size", (object[] args) => (object)10).Execute();
+
+        var inspector = new QuadrantInspector();
+        var mockColliding = new Mock<IColliding>();
+
+        mockColliding.SetupGet(o => o.Shape).Returns("shape");
+
+        var isInQuadrant = inspector.InQuadrant(mockColliding.Object, new[] { 1, 0 });
+
+        Assert.True(isInQuadrant);
+    }
+
+    [Fact]
+    public void CorrectFalseSetQuadrantsToObjectTest()
+    {
+        var key = "shape";
+
+        var polygon = new Polygon(new List<Point>() { new Point(1, 1), new Point(2, 2), new Point(11, 3) });
+
+        var polygonDict = new Dictionary<string, Polygon> {
+                            { key, polygon }
+                        };
+
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "Collision.GetPolygonDict",
+            (object[] args) => polygonDict
+        ).Execute();
+
+        Ioc.Resolve<App.ICommand>("IoC.Register", "Field.Quadrant.Size", (object[] args) => (object)10).Execute();
+
+        var inspector = new QuadrantInspector();
+        var mockColliding = new Mock<IColliding>();
+
+        mockColliding.SetupGet(o => o.Shape).Returns("shape");
+
+        var isInQuadrant = inspector.InQuadrant(mockColliding.Object, new[] { 0, 1 });
+
+        Assert.False(isInQuadrant);
+    }
+
+    [Fact]
+    public void CorrectObjectsInQuadrantTest()
+    {
+        var key1 = "shape1";
+        var key2 = "shape2";
+        var key3 = "shape3";
+
+        var polygon1 = new Polygon(new List<Point>() { new Point(1, 1), new Point(2, 2), new Point(11, 3) });
+        var polygon2 = new Polygon(new List<Point>() { new Point(5, 5), new Point(3, 7),
+            new Point(5, 9), new Point(6, 7) });
+        var polygon3 = new Polygon(new List<Point>() { new Point(13, 3), new Point(12, 6),
+            new Point(13, 7), new Point(14, 6), new Point(15, 4) });
+
+        var polygonDict = new Dictionary<string, Polygon> {
+                            { key1, polygon1 },
+                            { key2, polygon2 },
+                            { key3, polygon3 },
+                        };
+
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "Collision.GetPolygonDict",
+            (object[] args) => polygonDict
+        ).Execute();
+
+        Ioc.Resolve<App.ICommand>("IoC.Register", "Field.Quadrant.Size", (object[] args) => (object)10).Execute();
+
+        var inspector = new QuadrantInspector();
+        var mockColliding1 = new Mock<IColliding>();
+        var mockColliding2 = new Mock<IColliding>();
+        var mockColliding3 = new Mock<IColliding>();
+
+        mockColliding1.SetupGet(o => o.Shape).Returns("shape1");
+        mockColliding2.SetupGet(o => o.Shape).Returns("shape2");
+        mockColliding3.SetupGet(o => o.Shape).Returns("shape3");
+
+        var colList = new List<IColliding> { mockColliding1.Object, mockColliding2.Object, mockColliding3.Object };
+
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "Collision.GetCollisionObjectsList",
+            (object[] args) => colList
+        ).Execute();
+
+        var quadrantObjects = inspector.GetObjectsInSameSquare(new[] { 1, 0 });
+
+        Assert.Equal(new List<IColliding>() { mockColliding1.Object, mockColliding3.Object }, quadrantObjects);
+    }
+}
---
+++ SpaceBattle.Lib/QuadrantInspector.cs
@@ -0,0 +1,70 @@
+﻿using App;
+
+namespace SpaceBattle.Lib;
+
+public class QuadrantInspector
+{
+    private readonly object quadrantSize;
+
+    public QuadrantInspector()
+    {
+        quadrantSize = Ioc.Resolve<int>("Field.Quadrant.Size");
+    }
+
+    public static bool Contain(List<int[]> list, int[] elem)
+    {
+        foreach (int[] el in list)
+        {
+            if (el[0] == elem[0] && el[1] == elem[1])
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public List<int[]> getQuadrants(IColliding obj)
+    {
+        var shapesDict = (IDictionary<string, Polygon>)Ioc.Resolve<object>("Collision.GetPolygonDict");
+        var polygon = shapesDict[obj.Shape];
+        List<int[]> quadrants = new List<int[]>();
+
+        foreach (Point point in polygon.Points)
+        {
+            var xCoord = (int)point.X / (int)quadrantSize;
+            var yCoord = (int)point.Y / (int)quadrantSize;
+
+            int[] qCoords = { xCoord, yCoord };
+            if (!Contain(quadrants, qCoords))
+            {
+                quadrants.Add(qCoords);
+            }
+        }
+
+        return quadrants;
+    }
+
+    public bool InQuadrant(IColliding obj, int[] quadrantCoords)
+    {
+        var quadrants = getQuadrants(obj);
+
+        return Contain(quadrants, quadrantCoords);
+    }
+
+    public List<IColliding> GetObjectsInSameSquare(int[] quadrantCoords)
+    {
+        var collidingObjectsList = (IList<IColliding>)Ioc.Resolve<object>("Collision.GetCollisionObjectsList");
+        List<IColliding> nearObjects = new List<IColliding>();
+        foreach (IColliding cObj in collidingObjectsList)
+        {
+            var objectQuadrants = getQuadrants(cObj);
+            if (Contain(objectQuadrants, quadrantCoords))
+            {
+                nearObjects.Add(cObj);
+            }
+        }
+
+        return nearObjects;
+    }
+}
