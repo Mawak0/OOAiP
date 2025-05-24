0046661ad38a2918c5f66b874db9ff3791c14c29
MarkSabenin <sabenin-mark@mail.ru>
2025-05-25 00:07:01 +0600

    feat: SquareInspector

---
+++ SpaceBattle.Lib/QuadrantInspector.cs
@@ -0,0 +1,43 @@
+﻿using App;
+
+namespace SpaceBattle.Lib;
+
+public class QuadrantInspector
+{
+    private readonly object quadrantSize;
+
+    public QuadrantInspector()
+    {
+        quadrantSize = Ioc.Resolve<int>("Field.Quadrant.Size");
+    }
+
+    public static bool Contain(List<int[]> list, int[] elem)
+    {
+        return list.Any(el => el.SequenceEqual(elem));
+    }
+
+    public List<int[]> getQuadrants(IColliding obj)
+    {
+        var shapesDict = (IDictionary<string, Polygon>)Ioc.Resolve<object>("Collision.GetPolygonDict");
+        var polygon = shapesDict[obj.Shape];
+
+        return polygon.Points
+                .Select(point =>
+                    (
+                        (int)point.X / (int)quadrantSize,
+                        (int)point.Y / (int)quadrantSize
+                    ))
+                .Distinct()
+                .Select(tuple => new[] { tuple.Item1, tuple.Item2 })
+                .ToList();
+    }
+
+    public List<IColliding> GetObjectsInSameSquare(int[] quadrantCoords)
+    {
+        var collidingObjectsList = (IList<IColliding>)Ioc.Resolve<object>("Collision.GetCollisionObjectsList");
+
+        return collidingObjectsList.Where(
+                  cObj => Contain(getQuadrants(cObj), quadrantCoords)
+              ).ToList();
+    }
+}
