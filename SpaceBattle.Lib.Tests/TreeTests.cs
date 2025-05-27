cad712a04df0240e368b923785ccf67e6379169f
bunpdy <bunpdyz@mail.ru>
2025-05-27 21:39:01 +0600

    feat: CollisionDataStorage added

---
+++ SpaceBattle.Lib.Tests/TreeTests.cs
@@ -0,0 +1,42 @@
+﻿namespace SpaceBattle.Lib.Tests;
+
+using Xunit;
+
+public class TreeTests
+{
+    [Fact]
+    public void ContainsFourTest()
+    {
+        var four = new List<(int, int, int, int)> { (4, 6, 8, 3) };
+        var t = new Tree(four);
+
+        Assert.True(t.Contains((4, 6, 8, 3)));
+    }
+
+    [Fact]
+    public void NotContainsFourTest()
+    {
+        var four = new List<(int, int, int, int)> { (4, 6, 8, 3) };
+        var t = new Tree(four);
+
+        Assert.False(t.Contains((2, 6, 8, 3)));
+    }
+
+    [Fact]
+    public void EmptyTreeTest()
+    {
+        var four = new List<(int, int, int, int)>();
+        var t = new Tree(four);
+
+        Assert.False(t.Contains((4, 6, 8, 3)));
+    }
+
+    [Fact]
+    public void LastComparationTest()
+    {
+        var four = new List<(int, int, int, int)> { (5, 8, 4, 1) };
+        var t = new Tree(four);
+
+        Assert.False(t.Contains((5, 8, 4, 2)));
+    }
+}
---
+++ SpaceBattle.Lib/AddToStorageCommand.cs
@@ -0,0 +1,22 @@
+﻿namespace SpaceBattle.Lib;
+
+public class AddToStorageCommand : App.ICommand
+{
+    private readonly Tree tree;
+    private readonly string fig1;
+    private readonly string fig2;
+    private readonly Dictionary<(string, string), Tree> storage;
+
+    public AddToStorageCommand(string f1, string f2, Tree t, Dictionary<(string, string), Tree> s)
+    {
+        fig1 = f1;
+        fig2 = f2;
+        tree = t;
+        storage = s;
+    }
+
+    public void Execute()
+    {
+        storage[(fig1, fig2)] = tree;
+    }
+}
