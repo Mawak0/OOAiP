4d58c8e8117aa5da2da0c4cf49a2f63a7c475002
ShelDenis <shele1-2010@mail.ru>
2025-05-27 19:11:12 +0600

    fix: quadrant dict

--- SpaceBattle.Lib/QuadrantInspector.cs
+++ SpaceBattle.Lib/QuadrantInspector.cs
@@ -5,16 +5,15 @@ namespace SpaceBattle.Lib;
 public class QuadrantInspector
 {
     private readonly object quadrantSize;
+    private readonly Dictionary<(int, int), List<string>> quadrantField;
 
     public QuadrantInspector()
     {
         quadrantSize = Ioc.Resolve<int>("Field.Quadrant.Size");
+        quadrantField = new Dictionary<(int, int), List<string>>();
     }
 
-    public static bool Contain(List<int[]> list, int[] elem)
-    {
-        return list.Any(el => el.SequenceEqual(elem));
-    }
+    public Dictionary<(int, int), List<string>> QuadrantField => quadrantField;
 
     public List<int[]> getQuadrants(IColliding obj)
     {
@@ -32,11 +31,41 @@ public class QuadrantInspector
                 .ToList();
     }
 
+    public void AddObjectToField(IColliding obj)
+    {
+        var quadrants = getQuadrants(obj);
+
+        quadrants.ToList().ForEach(q =>
+        {
+            var key = (q[0], q[1]);
+            if (!quadrantField.TryGetValue(key, out var shapes))
+            {
+                quadrantField[key] = new List<string>();
+            }
+
+            quadrantField[key].Add(obj.Shape);
+        });
+    }
+
+    public void DeleteObjectFromField(IColliding obj)
+    {
+        var quadrants = getQuadrants(obj);
+
+        quadrants.Where(q => quadrantField.ContainsKey((q[0], q[1]))).ToList()
+            .ForEach(q => quadrantField[(q[0], q[1])].Remove(obj.Shape));
+    }
+
+    public void UpdateObjectOnField(IColliding obj, IColliding newObj)
+    {
+        DeleteObjectFromField(obj);
+        AddObjectToField(newObj);
+    }
+
     public List<IColliding> GetObjectsInSameSquare(int[] quadrantCoords)
     {
-        var collidingObjectsList = (IList<IColliding>)Ioc.Resolve<object>("Collision.GetCollisionObjectsList");
+        var figuresInQuadrant = quadrantField[(quadrantCoords[0], quadrantCoords[1])];
+        var collidingDict = Ioc.Resolve<Dictionary<string, IColliding>>("Collision.GetCollisionObjectsDict");
 
-        return collidingObjectsList.Where(cObj => Contain(getQuadrants(cObj), quadrantCoords)
-              ).ToList();
+        return figuresInQuadrant.Select(key => collidingDict[key]).ToList();
     }
 }
